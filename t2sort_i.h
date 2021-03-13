61d3a7e5de9c6d3623561a45a6af3c15547c55c9
yangkunlun <yangkunlun@outlook.com>
2021-03-13 18:58:06 +0800

    clean up the comments.

--- t2sort_i.h
+++ t2sort_i.h
@@ -14,47 +14,35 @@
 #include <fcntl.h>
 #include "t2sort.h"
 
+typedef t2sort_key_def_t t2_kdef_t;
+
 #define MIN(a,b)    ((a)>(b)?(b):(a))
 #define MAX(a,b)    ((a)>(b)?(a):(b))
 #define PAGE_SIZE   4096
 #define PAGE_MASK   12  //1<<PAGE_MASK==PAGE_SIZE
-/*
-typedef struct {
-    void      *_base;   //memory allocation start
-    size_t     _size;   //allocation size
-    void      * p;      //trace start, p>_base
-    int         ntr;    //number of trace, capacity
-    //p+ntr*trln may ge _base+_size, but less than a page
-} t2_mem_t ;    //_base->p is to save spill over
-
-typedef struct t2_blk_struct t2_blk_t;
-struct t2_blk_struct {
-    t2_blk_t  * prev, *next;  
-    t2_mem_t    mem[2];
-}; */
 
 typedef struct t2sort_aio_struct {
-    struct aiocb        paio;
+    struct aiocb    paio;
 } t2_aio_t;
 
 typedef union t2sort_pay_struct {
-    void              * ptr;    //pointer
-    int64_t             idx;    //index
-    struct { int32_t    blk,idx;}bpi;  //Block-Pile-Information
+    void      * ptr;    //pointer
+    int64_t     idx;    //index
+    struct { 
+        int32_t blk,idx;}
+                bpi;  //Block-Pile-Information
 } t2_pay_t;
 
-//queue generation algorith ensure ntr aligned with block size
-typedef struct t2sort_rque_struct t2_que_t;
-struct t2sort_rque_struct { //TODO: rename to t2_que_t
-    t2_que_t      * next;
-    t2_que_t      * prev;
-    int                 ntr;
-    int                 blk;    //read from which disk block
-    //additional information for later aligned operation.
-    off_t               seek;   //trace index of seeking
-    t2_aio_t        aio;
-    int                 flag;
-    int                 id;
+typedef struct t2sort_que_struct t2_que_t;
+struct t2sort_que_struct {
+    t2_que_t  * next;
+    t2_que_t  * prev;
+    int         ntr;
+    int         blk;    //read from which disk block
+    off_t       seek;   //trace index of seeking
+    t2_aio_t    aio;
+    int         flag;
+    int         id;
 };
 #define T2SORT_RQUE_SUBMIT  (1<<0)
 #define T2SORT_RQUE_SPLIT   (1<<1)
@@ -62,35 +50,32 @@ struct t2sort_rque_struct { //TODO: rename to t2_que_t
 #define T2SORT_RQUE_FINISH  (1<<3)
 
 typedef struct t2sort_struct {
-    int                 fd;         //data file
-    int                 fd_keys;    //keys file, not used yet
-    int                 trln;
-    int                 wioq;
-    t2sort_key_def_t  * kdef;
-    int                 ndef;
-    int                 klen;
-    int                 flag;
-    char                fd_name[64];
-//////////////////////////////////////////////
-    int                 nblk;
-    int64_t             nkey;   //todo remove
-    int64_t             bntr;
-    int64_t             pntr;
-//////////////////////////////////////////////
-    t2_que_t        read;       //waiting que
-    t2_que_t        wait;      //replace one first!
-
-    int64_t             head;       //read key index
-    int64_t             tail;      //because of 2nd pass sort
-    int64_t             done;      //read already given to user
-    int64_t             nfly;      //readraw on the fly
-    int64_t             wrap;      //pntr*(wioq+1)
+    int         fd;         //data file
+    int         fd_keys;    //keys file, not used yet
+    int         trln;       //trace length in byte
+    int         wioq;       //nppb, Number of Piles Per Block, TODO: rename
+    t2_kdef_t * kdef;       //key definition
+    int         ndef;       //number of key definition
+    int         klen;       //key struct length
+    int         flag;
+    char        fd_name[64];
+    int         nblk;       //disk block count
+    int64_t     nkey;       //Total ntr count
+    int64_t     bntr;       //ntr per block
+    int64_t     pntr;       //ntr per pile
+    t2_que_t    read;       //IO pool queue, TODO: rename
+    t2_que_t    wait;       //IO wait queue
+    int64_t     head;       //IO head index
+    int64_t     tail;       //IO tail index
+    int64_t     done;       //IO done index
+    int64_t     nfly;       //IO flying
+    int64_t     wrap;       //ring buffer size pntr*(wioq+1)
 //////////////////////////////////////////////
     int  (*func_cmp_key)(const void*,const void*);
-    void (*func_cpy_key)(void*,int,int,const t2sort_key_def_t*,void*);
+    void (*func_cpy_key)(void*,int,int,const t2_kdef_t*,void*);
 //////////////////////////////////////////////
-    void              *_base;
-    t2_que_t      *_xque;
+    void      *_base;
+    t2_que_t  *_xque;
 } t2sort_t;
 
 int dbg_gen_key(int kmin, int kmax);
