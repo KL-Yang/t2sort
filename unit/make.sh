aabceb0ab1fc7021f09ca84e120692c63aa0a53c
yangkunlun <yangkunlun@outlook.com>
2021-03-17 13:49:58 +0800

    refine the unit test-3

--- unit/make.sh
+++ unit/make.sh
@@ -2,13 +2,16 @@
 
 flag="-W -Wall -O3 -ggdb"
 
+python3 ../gentype.py header > ../gentype_gen.h
+python3 ../gentype.py debug  > ../gentype_gen.c
+
 #gcc $flag unit5_test1.c -o unit5_test1 -lm -lrt ../t2sort.o 
 #./unit5_test1
 #exit
 
 ########## unit test-3
-gcc $flag unit3_type.c -o unit3
-#./unit3
+gcc $flag -Wno-unused-function unit3_type.c -o unit3
+./unit3
 exit
 
 ########## unit test-2
@@ -20,8 +23,6 @@ time ./unit2 50000000 2
 exit
 
 ########## unit test-1
-python3 ../gentype.py header > gentype_gen.h
-python3 ../gentype.py debug  > gentype_gen.c
 gcc -c $flag unit1_map.c 
 gcc $flag -o unit1_map unit1_map.o -lm
 for ninst in {2001..10000..1333}; do
--- unit/unit3_type.c
+++ unit/unit3_type.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-
-#include "x_del_unit3_type.h"
-#include "x_del_unit3_type.c"
-
-static int t2sort_index1(int t1)
-{   return t1*21;           } 
-static int t2sort_index2(int t1, int t2)
-{   return t1*21+t2*5+1;    }
-static int t2sort_index3(int t1, int t2, int t3)
-{   return t1*21+t2*5+t3+2; }
+#include "../t2sort_i.h"
+#include "gentype_gen.h"
+#include "gentype_gen.c"
+#include "../gentype.c"
 
 int main()
 {
-    int x, i[2], t[3];
-    x = 0; i[0] = 5; i[1] = 7;
-    cmp_funcs[0](&i[0], &i[1]);
-    cpy_funcs[0](&i[0], &x, &i[1]);
+    int x, t[3]; t2_kdef_t kdef[3];
+
     //test for 1-key only
     for(t[0]=0; t[0]<T2SORT_NTYPE; t[0]++) {
-        x = t2sort_index1(t[0]);
+        kdef[0].type = t[0];
+        x = t2sort_get_func_index(1, kdef);
         printf("1@type=%d index=%d\n", t[0], x);
         dbg_funcs[x](t);
     }
     //test for 2-key only
-    for(t[0]=0; t[0]<T2SORT_NTYPE; t[0]++)
+    for(t[0]=0; t[0]<T2SORT_NTYPE; t[0]++) {
+        kdef[0].type = t[0];
         for(t[1]=0; t[1]<T2SORT_NTYPE; t[1]++) {
-            x = t2sort_index2(t[0], t[1]);
+            kdef[1].type = t[1];
+            x = t2sort_get_func_index(2, kdef);
             printf("2@type=%d,%d index=%d\n", t[0], t[1], x);
             dbg_funcs[x](t);
+        } 
     }
-    //test for 2-key only
-    for(t[0]=0; t[0]<T2SORT_NTYPE; t[0]++)
-        for(t[1]=0; t[1]<T2SORT_NTYPE; t[1]++)
+    //test for 3-key only
+    for(t[0]=0; t[0]<T2SORT_NTYPE; t[0]++) {
+        kdef[0].type = t[0];
+        for(t[1]=0; t[1]<T2SORT_NTYPE; t[1]++) {
+            kdef[1].type = t[1];
             for(t[2]=0; t[2]<T2SORT_NTYPE; t[2]++) {
-                x = t2sort_index3(t[0], t[1], t[2]);
+                kdef[2].type = t[2];
+                x = t2sort_get_func_index(3, kdef);
                 printf("2@type=%d,%d,%d index=%d\n", t[0], t[1], t[2], x);
                 dbg_funcs[x](t);
             }
+        }
+    }
     printf("Unit test successful!\n");
     return 0;
 }
