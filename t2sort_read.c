ce1be6b3ab62768777ad4e6827507b4f7d5867fb
yangkunlun <yangkunlun@outlook.com>
2021-03-20 17:07:34 +0800

    simplify code.

--- t2sort_read.c
+++ t2sort_read.c
@@ -2,22 +2,22 @@
 #define C_T2SORT_READ_T2SORT
 
 static int 
-t2_wait_rblock(t2sort_t *h, t2_que_t *wait, int bntr, t2_que_t *done)
+t2_wait(t2sort_t *h, t2_que_t *wait, int bntr, t2_que_t *done)
 {
     int ntr=0;
     while(wait->next!=wait && ntr<bntr) {
-        t2_que_t *xque=xque_deque(wait);
-        t2_aio_wait(&xque->aio, 1);
-        if(xque->ma!=xque->Ma)
-            memcpy(h->_base+xque->ma%h->_wrap,
-                    h->_rblk[xque->blk].page, xque->Ma-xque->ma);
-        if(xque->Mz!=xque->mz)
-            memcpy(h->_rblk[xque->blk].page, 
-                h->_base+xque->mz%h->_wrap, xque->Mz-xque->mz);
-        h->func_cpy_key(h->_base+xque->ma%h->_wrap, h->trln, 
-                xque->ntr, h->kdef, h->_pkey+ntr*h->klen);
-        ntr += xque->ntr;
-        xque_enque(done, xque);
+        t2_que_t *x=xque_deque(wait);
+        t2_aio_wait(&x->aio, 1);
+        if(x->ma!=x->Ma)
+            memcpy(h->_base+x->ma%h->_wrap,
+                    h->_rblk[x->blk].page, x->Ma-x->ma);
+        if(x->Mz!=x->mz)
+            memcpy(h->_rblk[x->blk].page, 
+                    h->_base+x->mz%h->_wrap, x->Mz-x->mz);
+        h->func_cpy_key(h->_base+x->ma%h->_wrap, h->trln, 
+                x->ntr, h->kdef, h->_pkey+ntr*h->klen);
+        ntr += x->ntr;
+        xque_enque(done, x);
     };
     return ntr;
 }
@@ -31,7 +31,7 @@ void * t2sort_readraw(t2sort_t *h, int *ntr)
         xque_deque(&h->done);
     //make sure submit not overrun on buffers
     if(h->done.next==&h->done) { //data exhausted, sort a block!
-        int nsort = t2_wait_rblock(h, &h->wait, h->bntr, &h->done);
+        int nsort = t2_wait(h, &h->wait, h->bntr, &h->done);
         t2_sort_block(h, h->_pkey, nsort);
     }
     //Check if done queue left room, submit new read!
