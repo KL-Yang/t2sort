5005e4a1922794814dd1d7cd734caa47817c82fa
yangkunlun <yangkunlun@gmail.com>
2021-06-22 17:13:36 +0800

    Add pre-generated header and source file

--- make.sh
+++ make.sh
@@ -4,4 +4,4 @@ flag="-W -Wall -O0 -ggdb -D_GNU_SOURCE"
 python3 gentype.py header > gentype_gen.h
 python3 gentype.py > gentype_gen.c
 gcc -c $flag t2sort.c
-rm -f gentype_gen.c
+#rm -f gentype_gen.c
