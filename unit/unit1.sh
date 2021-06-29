8c8d00faf0b11de64f2135ecbdeb57852abc3000
yangkunlun <yangkunlun@gmail.com>
2021-06-29 09:44:55 +0800

    refactor unit1.sh for test

--- unit/unit1.sh
+++ unit/unit1.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/bash
 
-flag="-W -Wall -O0 -ggdb -std=gnu99"
 rm -f unit1
-########## unit test-1
+python3 ../gentype.py header > gentype_gen.h
+python3 ../gentype.py debug  > gentype_gen.c
+
+flag="-W -Wall -O3 -ggdb -std=gnu99"
+########## unit test-1 #########################
 gcc -c $flag unit1_map.c 
 gcc $flag -o unit1 unit1_map.o -lm
+
 for ninst in {2001..10000..1333}; do
     for trlen in {1120..6666..400}; do
+        echo "./unit1 $trlen $ninst"
         ./unit1 $trlen $ninst
+	if [ $? -ne 0 ]; then
+		exit
+	fi
     done
 done
 
